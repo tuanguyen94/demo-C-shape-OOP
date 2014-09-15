84ee0a9ba7e72b8efd2b86194fef993db0cfc134
tuanguyen94 <tuanguyen94@gmail.com>
2014-09-15 22:56:04 +0700

    first upload by Tuanguyen

---
+++ ConsoleApplication15/point.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+
+namespace ConsoleApplication15
+{
+    public class point : shape
+    {
+        private double x;
+        private double y;
+        public double hoanh_do
+        {
+            get
+            {
+                return this.x;
+            }
+            set
+            {
+                this.x = value;
+            }
+        }
+        public double tung_do
+        {
+            get
+            {
+                return this.y;
+            }
+            set
+            {
+                this.y = value;
+            }
+        }
+        public point()
+        { }
+        public point(double a,double b)
+        {
+            this.x=a;
+            this.y=b;
+        }
+        public override void noti()
+        {
+            Console.WriteLine("Toa do diem vua nhap [{0:0.0};{1:0.0}]",this.x,this.y);
+        }
+
+    }
+}
